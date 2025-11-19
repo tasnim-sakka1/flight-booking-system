9449cda5bae15710b1d1e64f2c11600bab9f7b32
tasnim-sakka1 <tasnim.sakka01@gmail.com>
2025-11-19 11:35:50 +0100

    custom api exception

--- src/main/java/com/iit/flightbooking/repositories/TravelerRepository.java
+++ src/main/java/com/iit/flightbooking/repositories/TravelerRepository.java
@@ -2,11 +2,23 @@ package com.iit.flightbooking.repositories;
 
 import com.iit.flightbooking.entities.Traveler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * Traveler Repository Interface
  *
  * @author Tasnim
  * @since 2025-10
  */
+@Repository
 public interface TravelerRepository extends JpaRepository<Traveler, Long> {
+
+    Traveler findByEmail(String email);
+    Traveler findByPassportNo(String passportNo);
+
+    // These are useful for update validation
+    boolean existsByEmailAndIdNot(String email, Long id);
+    boolean existsByPassportNoAndIdNot(String passportNo, Long id);
+
+
 }
